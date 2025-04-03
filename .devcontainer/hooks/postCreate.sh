0aece1cda537bd8cc040825337ee1c54634139bf
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2025-04-03 06:19:03 +0000

    feat: init arduino bridge for arm using platformio

--- .devcontainer/hooks/postCreate.sh
+++ .devcontainer/hooks/postCreate.sh
@@ -6,3 +6,4 @@ sudo rm -f /etc/apt/apt.conf.d/docker-clean
 echo 'Binary::apt::APT::Keep-Downloaded-Packages "true";' | sudo tee /etc/apt/apt.conf.d/keep-cache > /dev/null
 
 echo "source /root/ws_moveit/install/setup.sh" >> /root/.bashrc
+sudo apt-get install -y python3-venv
