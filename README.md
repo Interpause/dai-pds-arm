7da1d9ecb00cf69ca4ef3e9c0139cbcd04c1b38c
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2025-04-07 12:44:52 +0000

    feat: launch file for controlling arm

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 ```sh
 ros2 launch moveit_setup_assistant setup_assistant.launch.py
+ros2 launch arm_moveit_config demo.launch.py
+ros2 launch arm_arduino_bridge ctrl.launch.py
 ```
---
+++ src/arm_arduino_bridge/launch/ctrl.launch.py
@@ -0,0 +1,23 @@
+"""Launch file to launch the moveit rviz demo as the control UI while controlling the actual servos."""
+
+from ament_index_python.packages import get_package_share_path
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    pkg_arm_arduino_bridge = get_package_share_path("arm_arduino_bridge")
+    pkg_arm_moveit_config = get_package_share_path("arm_moveit_config")
+
+    node_bridge = Node(package="arm_arduino_bridge", executable="bridge")
+    launch_moveit = IncludeLaunchDescription(
+        str(pkg_arm_moveit_config / "launch" / "demo.launch.py")
+    )
+
+    ld.add_action(node_bridge)
+    ld.add_action(launch_moveit)
+
+    return ld
