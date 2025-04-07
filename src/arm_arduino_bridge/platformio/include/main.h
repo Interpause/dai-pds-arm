8af0a5250bc3d5a68ecd795b2db7f3316deb50b0
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2025-04-07 11:25:25 +0000

    feat: http protocol for servo control

--- src/arm_arduino_bridge/platformio/include/main.h
+++ src/arm_arduino_bridge/platformio/include/main.h
@@ -1,16 +1,13 @@
 #include <Arduino.h>
 #include <Adafruit_PWMServoDriver.h>
 #include <WiFi.h>
+#include <ESPAsyncWebServer.h>
 #include "env.h"
 
 #define SERVO_FREQ 50
 #define SERVO_MIN_PULSE 500
-#define SERVO_MIN_ANGLE -90
 #define SERVO_MAX_PULSE 2500
-#define SERVO_MAX_ANGLE 90
-
-/** From -90 to 90 degrees. */
-void setAngle(uint8_t chan, int16_t ang);
+#define NUM_SERVOS 16
 
 /** Connect to wifi. */
 void connect();
--- src/arm_arduino_bridge/platformio/src/main.cpp
+++ src/arm_arduino_bridge/platformio/src/main.cpp
@@ -1,20 +1,22 @@
 #include "main.h"
 
 Adafruit_PWMServoDriver pwm;
+AsyncWebServer server(80);
+int16_t servoPulses[NUM_SERVOS];
 
-void setAngle(uint8_t chan, int16_t ang)
+void writeServos()
 {
-  int16_t us = map(ang, SERVO_MIN_ANGLE, SERVO_MAX_ANGLE, SERVO_MIN_PULSE, SERVO_MAX_PULSE);
-  us = constrain(us, SERVO_MIN_PULSE, SERVO_MAX_PULSE);
-  Serial.print("Setting angle: ");
-  Serial.println(us);
-  pwm.writeMicroseconds(chan, us);
+  for (auto i = 0; i < NUM_SERVOS; i++)
+  {
+    int16_t us = constrain(servoPulses[i], SERVO_MIN_PULSE, SERVO_MAX_PULSE);
+    Serial.printf("Servo %d: %d\n", i + 1, us);
+    pwm.writeMicroseconds(i, us);
+  }
 }
 
 void connect()
 {
-  Serial.print("SSID: ");
-  Serial.println(WIFI_SSID);
+  Serial.printf("SSID: %s\n", WIFI_SSID);
   Serial.print("Wifi connecting...");
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -24,11 +26,93 @@ void connect()
     Serial.print(".");
     delay(500);
   }
-  Serial.println("");
+  Serial.print("\n");
 
   Serial.println("Wifi connected!");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.printf("IP address: %s\n", WiFi.localIP().toString().c_str());
+  Serial.printf("Router address: %s\n", WiFi.gatewayIP().toString().c_str());
+  Serial.printf("DNS address: %s\n", WiFi.dnsIP().toString().c_str());
+  Serial.printf("Broadcast address: %s\n", WiFi.broadcastIP().toString().c_str());
+  neopixelWrite(RGB_BUILTIN, 0, RGB_BRIGHTNESS, 0);
+}
+
+void serverOnNotFound(AsyncWebServerRequest *req)
+{
+  if (req->method() == HTTP_OPTIONS)
+    req->send(200);
+  else
+    req->send(404);
+}
+
+void serverOnRead(AsyncWebServerRequest *req)
+{
+  Serial.printf("Read request.");
+  auto *res = req->beginResponseStream("application/json");
+
+  res->printf("[%d", servoPulses[0]);
+  for (auto i = 1; i < NUM_SERVOS; i++)
+    res->printf(",%d", servoPulses[i]);
+  res->print("]");
+  req->send(res);
+}
+
+void serverOnWrite(AsyncWebServerRequest *req)
+{
+  Serial.println("Write request.");
+  auto *res = req->beginResponseStream("text/plain");
+
+  auto n_params = req->params();
+  int16_t values[NUM_SERVOS];
+  bool invalid = false;
+  memcpy(values, servoPulses, sizeof(values));
+
+  for (auto i = 0; i < n_params; i++)
+  {
+    auto param = req->getParam(i);
+    auto name = param->name();
+    auto value = param->value();
+    auto id = name.toInt();
+    auto pulsewidth = value.toInt();
+    values[id - 1] = pulsewidth;
+
+    if (!name.endsWith("_servo"))
+    {
+      res->printf("Invalid param name: %s (Should be NN_servo)\n", name.c_str());
+      invalid = true;
+    }
+    if (id < 1 || id > NUM_SERVOS)
+    {
+      res->printf("Invalid servo id: %d (Should be 1 to %d)\n", id, NUM_SERVOS);
+      invalid = true;
+    }
+    if (pulsewidth < SERVO_MIN_PULSE || pulsewidth > SERVO_MAX_PULSE)
+    {
+      res->printf("Invalid pulsewidth for %s: %d (Should be between %d and %d)\n", name.c_str(), pulsewidth, SERVO_MIN_PULSE, SERVO_MAX_PULSE);
+      invalid = true;
+    }
+  }
+
+  if (invalid)
+  {
+    res->setCode(400);
+  }
+  else
+  {
+    res->print("OK");
+    memcpy(servoPulses, values, sizeof(values));
+    writeServos();
+  }
+
+  req->send(res);
+}
+
+void setupServer()
+{
+  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
+  server.on("/write", HTTP_GET, serverOnWrite);
+  server.on("/read", HTTP_GET, serverOnRead);
+  server.onNotFound(serverOnNotFound);
+  server.begin();
 }
 
 void setup()
@@ -38,12 +122,17 @@ void setup()
 
   pinMode(LED_BUILTIN, OUTPUT);
 
+  for (auto i = 0; i < NUM_SERVOS; i++)
+    servoPulses[i] = SERVO_MIN_PULSE;
+
   pwm.begin();
   pwm.setOscillatorFrequency(27000000);
   pwm.setPWMFreq(SERVO_FREQ);
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   connect();
+
+  setupServer();
 }
 
 void loop()
@@ -53,12 +142,5 @@ void loop()
     Serial.println("Wifi lost!");
     connect();
   }
-
-  neopixelWrite(RGB_BUILTIN, 0, RGB_BRIGHTNESS, 0);
-  setAngle(0, -90);
-  delay(5000);
-  neopixelWrite(RGB_BUILTIN, RGB_BRIGHTNESS, 0, 0);
-  setAngle(0, 90);
-  delay(5000);
   delay(10);
 }
